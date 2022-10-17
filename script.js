313fa251d4dd7d55872b29698ffb5c6660babae8
Chris McKenzie <kristopolous@yahoo.com>
2022-10-17 07:18:37 +0000

    adding a small script

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  let aTag = document.getElementsByTagName('a'), linkList = [];
+
+  for (var x = 0; x < aTag.length; x++) {
+    if (aTag[x].hasAttribute('href') && aTag[x].getAttribute('href').includes('http')) {
+      linkList.push(aTag[x].href.replace(/(_|\/|\?|&)/g, '$1<wbr>'));
+      aTag[x].innerHTML += "<span class='link onlyprint'> [L" + linkList.length + "] </span>";
+    } 
+  }
+  document.getElementById("print-links").innerHTML += "<ol><li>" + linkList.join("</li><li>") + "</li></ol>";
+}
